e7eb525630ee81fb2c35b7092a5774ae9e18d5df
Walace Nascimento <wlacens@hotmail.com>
2021-05-20 14:38:53 -0300

    Resolve o exercício 1

---
+++ Fundamentos/bloco-4/dia-1/exe01.js
@@ -0,0 +1,12 @@
+let a = 20;
+let b = 5;
+
+// console.log("Soma: " + (a+b));
+
+//console.log("Subtração: " + (a - b));
+
+//console.log("Multiplicação: " + (a * b));
+
+//console.log("Divisão: " + (a / b));
+
+// console.log("Modulo: " + (a + b));
